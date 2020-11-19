0659c4cdb074348efab383a8c5fd0d6f2c9dc003
Yuri <yuri_dsx_@hotmail.com>
2020-11-19 12:05:38 -0300

    Not bad but enough -> end

--- border-radius-previewer/app.js
+++ border-radius-previewer/app.js
@@ -3,20 +3,17 @@ const copy = document.getElementById('copy-text')
 
 
 function changeRadius(){
-    // console.log(box)
     const topLeft = document.getElementById('top-left').value 
     const topRight = document.getElementById('top-right').value
     const bottomLeft = document.getElementById('bottom-left').value
     const bottomRight = document.getElementById('bottom-right').value
 
-    // console.log(topLeft)
 
     const valueStyle = box.style.borderRadius = `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
     
     return valueStyle
 }
 
-// const valueResult
 function copyText(){
     const boxCopy = document.createElement("boxCopy")
     valueStyle = changeRadius()
